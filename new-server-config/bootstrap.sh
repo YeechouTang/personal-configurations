5ba76a3c472aadfa6903f47b02f88e128655fef9
Tang Yizhou <tangyeechou@gmail.com>
2022-07-07 17:48:22 +0800

    Install Vundle in bootstrap script

--- new-server-config/bootstrap.sh
+++ new-server-config/bootstrap.sh
@@ -9,4 +9,9 @@ curl -fo ~/.z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
 curl -fo ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 curl -fo ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
+echo "Start to install Vundle and its plugins for Vim..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+echo "Vundle installation completed!"
+
 source ~/.bashrc
